7df70083d3b8ab3cd089f1efae03cedb7c345df9
JuliusCaesar02 <69314234+JuliusCaesar02@users.noreply.github.com>
2020-08-26 18:01:24 +0200

    Add files via upload

--- Gui.java
+++ Gui.java
@@ -1,18 +1,12 @@
-import java.awt.EventQueue;
 import java.util.Arrays;
-
-
+import java.awt.*;  
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JButton;
-import javax.swing.JTable;
 import javax.swing.JTextArea;
 import javax.swing.JToggleButton;
-import javax.swing.JTextPane;
-import javax.swing.JFormattedTextField;
 public class Gui {
 	
 	private JFrame frame;
@@ -46,7 +40,12 @@ public class Gui {
 	 */
 	private void initialize() {
 		frame = new JFrame("Caesar subnet calculator");
-		frame.setBounds(100, 100, 452, 377);
+		Image icon = Toolkit.getDefaultToolkit().getImage("src//icon.png"); 
+		frame.setIconImage(icon);    
+		frame.setLayout(null);     
+		frame.setSize(500,500);     
+		frame.setVisible(true);  
+		frame.setBounds(100, 100, 454, 392);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		textField = new JTextField();
@@ -71,22 +70,35 @@ public class Gui {
 		
 		JTextArea textArea = new JTextArea();
 		textArea.setEditable(false);
-		textArea.setBounds(10, 60, 414, 267);
-		frame.getContentPane().add(textArea);
+		textArea.setBounds(10, 60, 414, 282);
+		frame.getContentPane().add(textArea); 
 		
 		tglbtnNewToggleButton.addActionListener(new ActionListener(){  
 		public void actionPerformed(ActionEvent e) {
 			String auxString;
 			auxString = new String();
 			String ip = textField.getText();
-			sub.setIp (ip);																										
-			sub.decon(sub.setIp(ip));																						
+			sub.setIp (ip);		
+			System.out.println(sub.decon(sub.setIp(ip))[0] );
+			if(sub.decon(sub.setIp(ip))[0] == -1) {
+				textArea.setText("È stata riscontrata un'eccezione! Verifica il formato dell'IP" +"\n");
+				return;	
+			}		
+			else if(sub.decon(sub.setIp(ip))[0] == -2) {
+				textArea.setText("Errore! L'IP inserito non esiste" +"\n");
+				return;	
+			}
 		    sub.setClasse();
 		    String sm = textField_1.getText();
 		    sub.setSm (sm);		
-		    sub.decon(sub.setIp(sm));																							//Separa la stringa in un'array intero con una posizione per byte
-
-		    
+			if(sub.decon(sub.setIp(sm))[0] == -1) {
+				textArea.setText("È stata riscontrata un'eccezione! Verifica il formato della SM" +"\n");
+				return;	
+			}
+			else if(sub.decon(sub.setIp(sm))[0] == -2) {
+				textArea.setText("Errore! La SM inserita non esiste" +"\n");
+				return;	
+			}
 		    sub.setBinaryBit(sub.decon(sub.setIp(ip)));		
 		    textArea.setText("Indirizzo IP binario: " +sub.getBinaryBit(sub.decon((sub.setIp(ip)))) +"\n");
 		    
@@ -96,10 +108,18 @@ public class Gui {
 		    sub.decon(sub.setIp(ip));
 		    textArea.append("1- La classe è " +sub.setClasse() +"\n");
 		    
+	    	if(sub.setClasse() == 'D' || sub.setClasse() == 'E') {
+	    		 textArea.append("Non è possibile eseguire ulteriori operazioni" +"\n");	 		
+	    		 return;	
+		    }
+			if(sub.setClasse() == 'X') {
+				 textArea.append("L'indirizzo IP inserito non esiste" +"\n");	 	
+	    		 return;	
+			}
 		    if(sub.setByteCrit() != 0) {
 		    	 textArea.append("2- Il byte critico è il " +(sub.setByteCrit()+1) +" (" +sub.decon(sm)[sub.setByteCrit()] +")" +"\n");
 			}
-			else textArea.append("2- Il byte critico non è presente (" +sub.decon(sm)[3] +")");
+			else textArea.append("2- Il byte critico non è presente (" +sub.decon(sm)[3] +")" +"\n");
 		    
 		    textArea.append("3- Il CIDR è /" +sub.setCidr() +"\n");
 		    
@@ -108,7 +128,7 @@ public class Gui {
 	    	sub.setSubBit();
 	    	if(sub.setSubBit() < 0) {
 	    		textArea.append("5- ERRORE! I bit di sotto rete sono negativi! " +sub.setSubBit() +"\n");
-				System.exit(0);
+	    		return;	
 			}
 			else textArea.append("5- I bit di sotto rete sono " +sub.setSubBit() +"\n");
 	    	 
@@ -138,12 +158,7 @@ public class Gui {
 
     		textArea.append("13- L'IP fa parte della subnet " +sub.setSubNet() +"\n");
 
-
-		    //textArea.append(sub.decon((sub.setIp(ip)))[0]);
-		    //lblNewLabel_2.appe
-		   // aux = sub.decon(sub.setIp(sm));
-		    //auxString = Arrays.toString(aux);
-		    //lblNewLabel_2.setText(auxString.replace(", ",".").replace("[","").replace("]",""));
+    		textArea.append(sub.setBitMap());
 		}
 		});
 	}
